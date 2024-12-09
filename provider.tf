6605d1f5f61d7baed10b81b3de9f6a41964edfc1
vitorrafael <vrsilveira07@gmail.com>
2024-12-08 21:20:46 -0300

    chore: use s3 backend instead of terraform cloud

--- provider.tf
+++ provider.tf
@@ -1,21 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = ">=5.35"
-    }
-  }
-  required_version = ">= 1.1.0"
-
-  cloud {
-    organization = "FIAP-SOAT-8-G6"
-
-    workspaces {
-      name = "vitorrafael-workspace"
-    }
-  }
-}
-
 provider "aws" {
   region = var.regionDefault
 }
