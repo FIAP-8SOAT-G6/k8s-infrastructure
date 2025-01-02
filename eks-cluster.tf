0064fae8a4cbaa054485bd88a71ed95b5abff908
Ana Paula Dezuo <ana.dezuo@rdstation.com>
2025-01-02 11:23:51 -0300

    fix: nomenclatures

--- eks-cluster.tf
+++ eks-cluster.tf
@@ -1,4 +1,4 @@
-resource "aws_eks_cluster" "eks-cluster" {
+resource "aws_eks_cluster" "eks_cluster" {
   name     = var.projectName
   role_arn = data.aws_iam_role.labrole.arn
 
@@ -12,8 +12,8 @@ resource "aws_eks_cluster" "eks-cluster" {
   }
 }
 
-resource "aws_eks_node_group" "node-group" {
-  cluster_name    = aws_eks_cluster.eks-cluster.name
+resource "aws_eks_node_group" "node_group" {
+  cluster_name    = aws_eks_cluster.eks_cluster.name
   node_group_name = "NG-${var.projectName}"
   node_role_arn   = data.aws_iam_role.labrole.arn
   subnet_ids      = [for subnet in data.aws_subnet.subnet : subnet.id if subnet.availability_zone != "${var.regionDefault}e"]
@@ -32,14 +32,14 @@ resource "aws_eks_node_group" "node-group" {
 }
 
 resource "aws_eks_access_entry" "access-entry" {
-  cluster_name      = aws_eks_cluster.eks-cluster.name
+  cluster_name      = aws_eks_cluster.eks_cluster.name
   principal_arn     = "arn:aws:iam::${var.accountId}:role/voclabs"
   kubernetes_groups = ["fiap"]
   type              = "STANDARD"
 }
 
 resource "aws_eks_access_policy_association" "eks-policy" {
-  cluster_name  = aws_eks_cluster.eks-cluster.name
+  cluster_name  = aws_eks_cluster.eks_cluster.name
   policy_arn    = var.policyArn
   principal_arn = "arn:aws:iam::${var.accountId}:role/voclabs"
 
