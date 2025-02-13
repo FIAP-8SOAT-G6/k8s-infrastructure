a646e957acc4fbfc938832b1ad8cfc86f517074a
vitorrafael <vrsilveira07@gmail.com>
2025-02-13 19:56:22 -0300

    chore: add public access control

--- eks/backend/s3.tf
+++ eks/backend/s3.tf
@@ -1,3 +1,29 @@
 resource "aws_s3_bucket" "backend" {
-  bucket = "tcl-terraform-bucket-apresentacao"
+  bucket = "tcl-terraform-bucket-vitorrafael"
+}
+
+resource "aws_s3_bucket_ownership_controls" "backend" {
+  bucket = aws_s3_bucket.backend.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "backend" {
+  bucket = aws_s3_bucket.backend.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket_acl" "example" {
+  depends_on = [
+    aws_s3_bucket_ownership_controls.backend,
+    aws_s3_bucket_public_access_block.backend,
+  ]
+
+  bucket = aws_s3_bucket.backend.id
+  acl    = "public-read"
 }
