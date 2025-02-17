b156f19e1c319ee0877a961c06e25911517eb849
Vítor Rafael Veeck da Silveira <vrsilveira07@gmail.com>
2025-02-16 22:17:15 -0300

    chore: database security groups and fix ingress service name (#19)

--- eks/security-groups.tf
+++ eks/security-groups.tf
@@ -26,13 +26,30 @@ resource "aws_security_group" "sg" {
     cidr_blocks = ["0.0.0.0/0"]
     description = "Allow outbound traffic to RDS"
   }
+
+  egress {
+    from_port   = 6379
+    to_port     = 6379
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Allow outbound traffic to Elasticache"
+  }
 }
 
-# resource "aws_security_group_rule" "eks_to_rds" {
-#   type                     = "ingress"
-#   from_port                = data.terraform_remote_state.rds.outputs.rds_port
-#   to_port                  = data.terraform_remote_state.rds.outputs.rds_port
-#   protocol                 = "tcp"
-#   security_group_id        = data.terraform_remote_state.rds.outputs.rds_security_group_id
-#   source_security_group_id = aws_security_group.sg.id
-# }
+resource "aws_security_group_rule" "eks_to_rds" {
+  type                     = "ingress"
+  from_port                = 5432
+  to_port                  = 5432
+  protocol                 = "tcp"
+  security_group_id        = data.terraform_remote_state.rds.outputs.rds_security_group_id
+  source_security_group_id = aws_security_group.sg.id
+}
+
+resource "aws_security_group_rule" "eks_to_elasticache" {
+  type                     = "ingress"
+  from_port                = 6379
+  to_port                  = 6379
+  protocol                 = "tcp"
+  security_group_id        = data.terraform_remote_state.elasticache.outputs.elasticache_security_group_id
+  source_security_group_id = aws_security_group.sg.id
+}
